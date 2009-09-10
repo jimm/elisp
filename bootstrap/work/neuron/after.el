a3d4da6974aa0526e925c7e1d3eb36857dfc1f11
Jim Menard <jimm@io.com>
2009-09-10 06:41:02 -0400

    work machine

---
+++ bootstrap/work/neuron/after.el
@@ -0,0 +1,43 @@
+(defun zoom-frame-width-cols ()
+  (interactive)				; for testing
+  268)                                  ; 268 for really full screen, 260 with dock exposed
+
+;; (setq sql-user "jimm")
+;; (setq sql-server "localhost")
+;; (setq sql-database "db")
+(setq sql-sqlite-program "sqlite3")
+
+(setq browse-url-generic-program "open")
+(setq Man-switches "-M /usr/share/man:/usr/local/share/man")
+
+(defvar compile-ant "ant -e -s build.xml ")
+(defvar compile-rake "rake test ")
+
+(set-register ?n compile-ant)
+(set-register ?q compile-rake)
+(setq compile-command (get-register ?q))
+
+(defvar sa-db
+  "/Users/jimm/src/sandbox/taxonomy/spine_align/db/development.sqlite3")
+(defvar st-db "/Users/jimm/src/sandbox/taxonomy/SpineTagger/spineTagger.db")
+(set-register ?d sa-db)
+(set-register ?e st-db)
+
+(custom-set-variables
+ '(pmd-java-home "/usr/bin/java"))
+
+(server-start)
+
+(global-set-key [\C-f4]
+  (lambda ()
+    (interactive)
+    (find-file (concat *my-pim-dir* "orgs/kaplan_todo.org"))))
+(global-set-key [f4]
+  (lambda ()
+    (interactive)
+    (find-file (concat *my-pim-dir* "orgs/todo.org"))))
+
+; TODO use light/dark versions code
+(set-face-attribute 'org-level-1 nil :height 140 :foreground "white" :background "black" :bold t)
+
+(set-cursor-color "cyan")
