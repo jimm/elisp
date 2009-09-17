10b529850d51997748b483225eb5ca2c503958ad
Jim Menard <jimm@io.com>
2009-09-17 11:47:54 -0400

    Use older, simpler javascript mode because I can't get js2-mode to compile in Emacs 23 on OS X.'

--- emacs.el
+++ emacs.el
@@ -426,8 +426,10 @@ and returns it."
 ;;
 ;; JavaScript
 ;;;
-(autoload 'js2-mode "js2-mode" nil t)
-(add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
+(autoload 'javascript-mode "javascript" nil t)
+(add-to-list 'auto-mode-alist '("\\.js$" . javascript-mode))
+;; (autoload 'js2-mode "js2-mode" nil t)
+;; (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
 
 (setq js2-basic-offset 2)
 ;(setq js2-use-font-lock-faces t)
