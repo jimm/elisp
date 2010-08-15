78eda6f561725fea47168707b4960e8125454651
Jim Menard <jimm@io.com>
2010-08-15 10:37:45 -0400

    Improved method for finding Erlang tools dir.

--- bootstrap/kaplan/neuron/before.el
+++ bootstrap/kaplan/neuron/before.el
@@ -14,7 +14,9 @@
 (add-to-list 'load-path "/usr/share/emacs/22.1/lisp" t)
 (add-to-list 'load-path "/usr/share/emacs/22.1/site-lisp" t)
 (defvar *my-erlang-emacs-tools-dir*
-  (concat (car (file-expand-wildcards "/opt/local/lib/erlang/lib/tools*"))
+  (concat (car (reverse
+                (sort (file-expand-wildcards "/opt/local/lib/erlang/lib/tools*") 'string-lessp)
+               ))
           "/emacs/"))
 
 (setq-default indent-tabs-mode nil)
