0ae69bf249dc79ffcb57af2fd781010259e5ed7b
brain <brain@qaapp14.eng.kaplan.com>
2010-09-03 16:01:42 -0400

    another work machine

---
+++ bootstrap/kaplan/qaapp14/after.el
@@ -0,0 +1,58 @@
+(defun zoom-frame-width-cols ()
+  (interactive)				; for testing
+  268)                                  ; 268 for really full screen, 260 with dock exposed
+
+(setq edit-server-new-frame-height 77)
+
+;; Standard Java indent level
+(add-hook 'java-mode-hook (lambda () (setq c-basic-offset 4)))
+
+;; this is dev-only info; won't work in production of course
+(setq sql-user "brain")                 ; Oracle dev instance
+(setq sql-password "brain")
+(setq sql-database "oraclexe")
+(setq sql-server "localhost")           ; MySQL
+(setq sql-sqlite-program "sqlite3")     ; SQLite
+
+(setq browse-url-generic-program "open")
+(setq Man-switches "-M /usr/share/man:/usr/local/share/man")
+
+(defvar compile-ant "ant -e -s build.xml ")
+(defvar compile-rake "rake test ")
+
+(set-register ?n compile-ant)
+(set-register ?q compile-rake)
+(setq compile-command (get-register ?q))
+
+(defvar sa-db
+  "/Users/jimm/src/spine_align/db/development.sqlite3")
+(defvar sa-db-perforce
+  "/Users/jimm/Documents/Perforce/brain/brain/spine_align/db/development.sqlite3")
+(defvar st-db
+  "/Users/jimm/src/sandbox/taxonomy/SpineTagger/spineTagger.db")
+(defvar st-db-perforce
+  "/Users/jimm/Documents/Perforce/prfny3a01_1666/Jim_Menard_neuron/depot/CoreTech/Brain/jimm_sandbox/taxonomy/SpineTagger/spineTagger.db")
+(set-register ?d sa-db)
+(set-register ?e st-db)
+
+(custom-set-variables
+ '(pmd-java-home "/usr/bin/java"))
+
+(server-start)
+
+(global-set-key [f4]
+  (lambda ()
+    (interactive)
+    (find-file (concat *my-pim-dir* "orgs/kaplan/todo.org"))))
+(global-set-key [\C-f4]
+  (lambda ()
+    (interactive)
+    (find-file (concat *my-pim-dir* "orgs/todo.org"))))
+(global-set-key [f6]
+  (lambda ()
+    (interactive)
+    (find-file (concat *my-pim-dir* "orgs/kaplan/notes.org"))))
+(global-set-key [\C-f6]
+  (lambda ()
+    (interactive)
+    (find-file (concat *my-pim-dir* "orgs/notes.org"))))
---
+++ bootstrap/kaplan/qaapp14/before.el
@@ -0,0 +1,22 @@
+(setq ns-command-modifier 'meta)        ; define Command as Meta key
+
+; Smoother mouse wheel scrolling
+; http://www.reddit.com/r/programming/comments/95uv7/emacs_231_has_been_released/c0bj6zp
+(setq mouse-wheel-scroll-amount (quote (1 ((shift) . 5) ((control)))))
+
+(when window-system
+  (defvar *basic-frame-alist* '((tool-bar-mode . nil)))
+  (setq initial-frame-alist
+	(append *basic-frame-alist* '((height . 77) (top . 0) (left . 0)
+                                      (cursor-color . "orange"))))
+  (setq default-frame-alist *basic-frame-alist*))
+
+(add-to-list 'load-path "/usr/share/emacs/22.1/lisp" t)
+(add-to-list 'load-path "/usr/share/emacs/22.1/site-lisp" t)
+(defvar *my-erlang-emacs-tools-dir*
+  (concat (car (reverse
+                (sort (file-expand-wildcards "/opt/local/lib/erlang/lib/tools*") 'string-lessp)
+               ))
+          "/emacs/"))
+
+(setq-default indent-tabs-mode nil)
