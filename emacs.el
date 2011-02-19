f8e4e2e27315647e763113552ef45c6e06c65af6
Jim Menard <jimm@io.com>
2011-02-18 20:46:58 -0500

    fixed coffescript mode file name

--- emacs.el
+++ emacs.el
@@ -553,7 +553,7 @@ and returns it."
 ;;
 ;; CoffeeScript
 ;;
-(autoload 'coffee-mode "coffee" nil t)
+(autoload 'coffee-mode "coffee-mode" nil t)
 (add-to-list 'auto-mode-alist '("\\.coffee$" . coffee-mode))
 (add-to-list 'auto-mode-alist '("Cakefile" . coffee-mode))
 
