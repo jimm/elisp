8ba08faaff694d6828dd4c707bdbb89634f8b183
Jim Menard <jim.menard@kaplan.com>
2011-06-06 12:25:40 -0400

    first checkin of new work config

---
+++ bootstrap/ideeli/stick/after.el
@@ -0,0 +1,102 @@
+(defun zoom-frame-width-cols ()
+  (interactive)				; for testing
+  268)                                  ; 268 for really full screen, 260 with dock exposed
+
+(setq edit-server-new-frame-height 77)
+
+(setq *my-javadoc-url*
+      "file:///Users/jimm/Documents/documentation/java-api/api/")
+
+;; Standard Java indent level
+(add-hook 'java-mode-hook (lambda () (setq c-basic-offset 4)))
+
+;; this is dev-only info; won't work in production of course
+(setq sql-user "sa_db_user")
+(setq sql-password "vertebrae")
+(setq sql-database "sa_development")
+(setq sql-server "localhost")           ; MySQL
+(setq sql-sqlite-program "sqlite3")     ; SQLite
+
+(setq browse-url-generic-program "open")
+(setq Man-switches "-M /usr/share/man:/usr/local/share/man")
+
+(defvar compile-ant "ant -e -s build.xml ")
+(defvar compile-rake "rake test ")
+(defvar compile-brain "cd $brain && bin/maven.sh ")
+
+(set-register ?n compile-ant)
+(set-register ?q compile-rake)
+(set-register ?m compile-brain)
+
+(setq compile-command compile-brain)
+
+(defvar sa-db
+  "/Users/jimm/src/spine_align/db/development.sqlite3")
+(defvar sa-db-perforce
+  "/Users/jimm/Documents/Perforce/brain/brain/spine_align/db/development.sqlite3")
+(defvar st-db
+  "/Users/jimm/src/sandbox/taxonomy/SpineTagger/spineTagger.db")
+(defvar st-db-perforce
+  "/Users/jimm/Documents/Perforce/prfny3a01_1666/Jim_Menard_neuron/depot/CoreTech/Brain/jimm_sandbox/taxonomy/SpineTagger/spineTagger.db")
+(set-register ?d sa-db)
+(set-register ?e st-db)
+
+(defun oraclexe (cmd)
+  (interactive "sCommand [start]: ")
+  (let ((c (if (> (length cmd) 0) cmd "start")))
+    (shell-command (concat "oraclexe " c))))
+
+(defun brain-play-test ()
+  (interactive)
+  (let ((eshell-buffer-name "play-server"))
+    (eshell)
+    (oraclexe "start")
+    (insert "cd $brain")
+    (eshell-send-input)
+    (insert "play test")
+    (eshell-send-input)))
+
+(defun sa-play-test ()
+  (interactive)
+  (let ((eshell-buffer-name "play-server"))
+    (eshell)
+    (insert "cd $sa")
+    (eshell-send-input)
+    (insert "play test")
+    (eshell-send-input)))
+
+(setq org-agenda-files (list (concat *my-pim-dir* "orgs/todo.org")
+			     (concat *my-pim-dir* "orgs/kaplan/todo.org")))
+
+;;
+;; Google Chrome edit server
+;;
+;(require 'edit-server)
+;(edit-server-start)
+; does not work; asks to kill process anyway
+;(add-hook 'kill-emacs-hook 'edit-server-stop)
+; does not work
+; (add-to-list 'kill-emacs-query-functions 'edit-server-stop)
+
+;; Start Emacs server
+(server-start)
+
+(global-set-key [f4]
+  (lambda ()
+    (interactive)
+    (find-file (concat *my-pim-dir* "orgs/kaplan/todo.org"))))
+(global-set-key [\C-f4]
+  (lambda ()
+    (interactive)
+    (find-file (concat *my-pim-dir* "orgs/todo.org"))))
+(global-set-key [f6]
+  (lambda ()
+    (interactive)
+    (find-file (concat *my-pim-dir* "orgs/kaplan/notes.org"))
+    (goto-char (point-max))))
+(global-set-key [\C-f6]
+  (lambda ()
+    (interactive)
+    (find-file (concat *my-pim-dir* "orgs/notes.org"))
+    (goto-char (point-max))))
+(global-set-key [f11] 'other-window)
