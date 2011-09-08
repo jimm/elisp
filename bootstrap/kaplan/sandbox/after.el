7c531a4933623e221b874e339e6b7a2b7f1e3fa4
Jim Menard <jim@jimmenard.com>
2011-09-08 14:58:11 -0400

    New default background color. Delete old unused work config settings.

--- bootstrap/kaplan/sandbox/after.el
+++
@@ -1,8 +0,0 @@
-(global-set-key [\C-f4]
-  (lambda ()
-    (interactive)
-    (find-file (concat *my-pim-dir* "orgs/kaplan.org"))))
-(global-set-key [f4]
-  (lambda ()
-    (interactive)
-    (find-file (concat *my-pim-dir* "orgs/todo.org"))))
