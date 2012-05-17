bc000f49c91df4a682a82dc829357e94caeaece7
Jim Menard <jim@jimmenard.com>
2012-05-16 22:07:01 -0400

    home machine colors

--- bootstrap/home/acadia/before.el
+++ bootstrap/home/acadia/before.el
@@ -11,11 +11,12 @@
 
 (when window-system
   (defvar *basic-frame-alist* '(; (background-color . "lavender")
-                                ; (cursor-color . "green")
+                                (cursor-color . "orange")
 				(tool-bar-mode . nil)))
   (setq initial-frame-alist
 	(append *basic-frame-alist* '((height . 76) (top . 0) (left . 0))))
-  (setq default-frame-alist *basic-frame-alist*))
+  (setq default-frame-alist *basic-frame-alist*)
+  (set-face-attribute 'mode-line nil :foreground "yellow" :background "black"))
 
 (add-to-list 'load-path "/usr/share/emacs/22.1/lisp" t)
 (add-to-list 'load-path "/usr/share/emacs/22.1/site-lisp" t)
