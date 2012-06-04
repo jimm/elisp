341a4e3dc8838cd8c623b5658b1f13df3f089063
Jim Menard <jim@generalassemb.ly>
2012-06-04 17:07:23 -0400

    changed a few key bindings

--- bootstrap/general_assembly/polyglot/after.el
+++ bootstrap/general_assembly/polyglot/after.el
@@ -19,28 +19,22 @@
 
 (add-to-list 'auto-mode-alist '("\\.\\(md\\|markdown\\)$" . markdown-mode))
 
-;;
-;; Google Chrome edit server
-;;
-;(require 'edit-server)
-;(edit-server-start)
-; does not work; asks to kill process anyway
-;(add-hook 'kill-emacs-hook 'edit-server-stop)
-; does not work
-; (add-to-list 'kill-emacs-query-functions 'edit-server-stop)
-
 ;; Start Emacs server
 (server-start)
 
-(global-set-key [\C-f4]
-  (lambda ()
-    (interactive)
-    (find-file (concat *my-pim-dir* "orgs/general_assembly/todo.org"))))
 (global-set-key [f4]
   (lambda ()
     (interactive)
+    (find-file (concat *my-pim-dir* "orgs/general_assembly/todo.org"))))
+(global-set-key [\C-f4]
+  (lambda ()
+    (interactive)
     (find-file (concat *my-pim-dir* "orgs/todo.org"))))
-(global-set-key [\C-f6]
+(global-set-key [f6]
   (lambda ()
     (interactive)
     (find-file (concat *my-pim-dir* "orgs/general_assembly/notes.org"))))
+(global-set-key [\C-f6]
+  (lambda ()
+    (interactive)
+    (find-file (concat *my-pim-dir* "orgs/notes.org"))))
