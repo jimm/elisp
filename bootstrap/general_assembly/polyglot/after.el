02650504fe1db0b008d756c2de580792be528ec5
Jim Menard <jim@jimmenard.com>
2012-07-03 22:40:18 -0400

    max screen width: bigger is better

--- bootstrap/general_assembly/polyglot/after.el
+++ bootstrap/general_assembly/polyglot/after.el
@@ -1,6 +1,6 @@
  (defun zoom-frame-width-cols ()
   (interactive)          ; for testing
-  268)                   ; 268 for really full screen, 260 with dock exposed
+  999)
 
 ;; (setq sql-user "jimm")
 ;; (setq sql-server "localhost")
