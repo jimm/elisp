bd2d71d8c69ad1a332b31f063019dd3b05fc59bf
Jim Menard <jim.menard@nrelate.com>
2013-04-01 10:17:44 -0400

    fixed nrepl key map initialization

--- progmodes/my-clojure-mode.el
+++ progmodes/my-clojure-mode.el
@@ -31,9 +31,12 @@
       (lambda ()
         (define-key clojure-mode-map "\r" 'newline-and-indent)
         (define-key clojure-mode-map "\C-cd" 'debug-comment)
+        (define-key clojure-mode-map "\C-ci" 'in-ns-to-inferior-lisp)
+        (define-key clojure-mode-map "\C-cn" 'ns-to-inferior-lisp)))
+
+(setq nrepl-connected-hook
+      (lambda ()
         ;; nREPL mode has two key bindings that do the same thing: \C-c\C-c
         ;; and C-M-x both run nrepl-eval-expression-at-point. Normally
         ;; \C-c\C-c is bound to comment-region, so let's reinstate that.
-        (define-key nrepl-interaction-mode-map "\C-c\C-c" 'comment-region)
-        (define-key clojure-mode-map "\C-ci" 'in-ns-to-inferior-lisp)
-        (define-key clojure-mode-map "\C-cn" 'ns-to-inferior-lisp)))
+        (define-key nrepl-interaction-mode-map "\C-c\C-c" 'comment-region)))
