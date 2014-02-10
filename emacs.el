74a863fd8cf5e4ff05b18d1067a916e85cb9b9ee
Jim Menard <jim@jimmenard.com>
2014-02-10 09:46:10 -0500

    use executable-interpret instead of run-buffer

--- emacs.el
+++ emacs.el
@@ -151,29 +151,6 @@ Goes backward if ARG is negative; error if CHAR not found."
   (insert-char char)
   (backward-char))
 
-(defun run-buffer (compile-cmd)
-  "Saves the current buffer to the file it's associated with, or
-  a temp file if no such file exists yet. Runs that file in the
-  compilation buffer using the given command.
-
-  The temp file, if created, is not deleted at the end of this
-  function because the compilation is performed asynchronously so
-  we don't want to delete the file before it's done being run."
-  (interactive "sCompile command:")
-  (if (file-exists-p (buffer-file-name))
-      (progn
-        (save-buffer)
-        (compile (concat compile-cmd " " (buffer-file-name))))
-    (progn
-      (let ((tmpfile (make-temp-file "emacs-region-" nil nil)))
-        (write-region nil nil tmpfile)
-        (compile (concat compile-cmd " " tmpfile))))))
-;; Can't remove temp file because compile is async and file could be deleted
-;; before it is used by compile.
-
-(defmacro run-buffer-with (compile-cmd)
-  `(lambda () (interactive) (run-buffer ,compile-cmd)))
-
 ;;
 ;; Display time and add time-related hooks
 ;;
@@ -452,7 +429,7 @@ the current directory, suitable for creation"
 ;;
 (add-hook 'sh-mode-hook
           '(lambda ()
-             (define-key sh-mode-map "\C-cr" (run-buffer-with ""))))
+             (define-key sh-mode-map "\C-cr" 'executable-interpret)))
 
 ;;
 ;; Subversion
@@ -594,7 +571,7 @@ the current directory, suitable for creation"
 (add-hook 'coffee-mode-hook
           '(lambda ()
              (setq coffee-js-mode 'javascript-mode)
-             (define-key coffee-mode-map "\C-cr" (run-buffer-with "coffee"))
+             (define-key coffee-mode-map "\C-cr" 'executable-interpret)
              (define-key coffee-mode-map "\C-ck" 'compile-coffee-buffer)
              (set (make-local-variable 'tab-width) 2)))
 
@@ -615,7 +592,7 @@ the current directory, suitable for creation"
           '(lambda ()
              (setq groovy-basic-offset 4)
              (define-key groovy-mode-map "\r" 'newline-and-indent)
-             (define-key groovy-mode-map "\C-cr" (run-buffer-with "groovy"))
+             (define-key groovy-mode-map "\C-cr" 'executable-interpret)
              (font-lock-mode 1)))
 
 ;; Groovy shell mode
@@ -796,7 +773,7 @@ the current directory, suitable for creation"
                  (define-key php-mode-map "\C-d" 'delete-char)
                  (define-key php-mode-map "\C-ct" 'html-mode)
                  (define-key php-mode-map "\C-ch" 'insert-ruby-hash-arrow)
-                 (define-key php-mode-map "\C-cr" (run-buffer-with "php -f"))))))
+                 (define-key php-mode-map "\C-cr" 'executable-interpret)))))
 
 ;;
 ;; HTML-mode and SGML-mode
@@ -951,7 +928,7 @@ the current directory, suitable for creation"
                 '(lambda ()
                    (define-key scala-mode-map [f1] my-shell) ; I don't use Speedbar
                    (define-key scala-mode-map "\r" 'newline-and-indent)
-                   (define-key scala-mode-map "\C-cr" (run-buffer-with "scala"))))
+                   (define-key scala-mode-map "\C-cr" 'executable-interpret)))
       ;; That bright red face for vars is too annoying
       (set-face-attribute 'scala-font-lock:var-face nil :bold nil :foreground "red3"))
   (error nil))
@@ -1031,7 +1008,7 @@ gzip.")))
 (add-hook 'python-mode-hook
           '(lambda ()
              (turn-on-font-lock)
-             (define-key python-mode-map "\C-cr" (run-buffer-with "python"))
+             (define-key python-mode-map "\C-cr" 'executable-interpret)
              ;; these two are in addition to the \C-< and \C-> bindings
              ;; that already exist in Python mode
              (define-key python-mode-map "\M-[" 'python-indent-shift-left)
