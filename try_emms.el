2697d6096745ce1fc5fbbf722336660b05b1892c
Jim Menard <jim@jimmenard.com>
2014-09-15 10:20:29 -0400

    try_emms key rebindings

--- try_emms.el
+++ try_emms.el
@@ -2,3 +2,13 @@
 (require 'emms-setup)
 (emms-standard)
 (emms-default-players)
+
+;; TODO Specific to my work setup. These rebindings should be moved into
+;; bootstrap/nrelate/widget/after.el.
+(when (fboundp 'vulcan-password-to-iterm)
+  (global-set-key [f5] 'vulcan-password-to-iterm)
+  (global-set-key [\C-f5] 'mindspark-password-to-clipboard))
+
+(global-set-key [f7] 'emms-previous)
+(global-set-key [f8] 'emms-pause)       ; toggles between pause and resume
+(global-set-key [f9] 'emms-next)
