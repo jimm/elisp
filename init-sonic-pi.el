01359d6fb621804fbcf9683350a4f00f9c411718
Jim Menard <jimm@io.com>
2014-11-25 13:26:42 -0500

    comment

--- init-sonic-pi.el
+++ init-sonic-pi.el
@@ -1,6 +1,14 @@
 ;;; See http://sonic-pi.net/ and
 ;;; https://github.com/repl-electric/sonic-pi.el
 
+;; M-x sonic-pi-mode
+;; M-x sonic-pi-jack-in
+;;     or, if already running sonic-pi-server
+;; M-x sonic-pi-connect
+
+;; To send buffer to sonic-pi, use C-c C-k
+;; To stop play, use C-c C-b
+
 (add-to-list 'load-path "~/.emacs.d/sonic-pi/")
 (require 'sonic-pi)
 (add-hook 'sonic-pi-mode-hook
