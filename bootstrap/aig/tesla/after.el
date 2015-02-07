e40a25a4c2bd16ee6f777670f09b87acb460e377
Jim Menard <jim@jimmenard.com>
2015-02-06 20:50:07 -0500

    work laptop

---
+++ bootstrap/aig/tesla/after.el
@@ -0,0 +1,46 @@
+;; (defun zoom-frame-width-cols ()
+;;   (interactive)          ; for testing
+;;   268)                   ; 268 for really full screen, 260 with dock exposed
+
+;; (setq sql-user "jimm")
+;; (setq sql-server "localhost")
+;; (setq sql-database "db")
+
+(setq dired-use-ls-dired nil)
+
+(setq browse-url-generic-program "open")
+(setq Man-switches "-M /usr/share/man:/usr/local/share/man")
+
+(defvar compile-ant "ant -e -s build.xml ")
+(defvar compile-rake "rake test ")
+
+(set-register ?n compile-ant)
+(set-register ?q "rake test ")
+
+(setq sql-sqlite-program "sqlite3")
+
+;;
+;; Google Chrome edit server
+;;
+;(require 'edit-server)
+;(edit-server-start)
+; does not work; asks to kill process anyway
+;(add-hook 'kill-emacs-hook 'edit-server-stop)
+; does not work
+; (add-to-list 'kill-emacs-query-functions 'edit-server-stop)
+
+;; Start Emacs server
+(server-start)
+
+(global-set-key [\C-f4]
+  (lambda ()
+    (interactive)
+    (find-file (concat *my-pim-dir* "orgs/work/aig/todo.org"))))
+(global-set-key [f4]
+  (lambda ()
+    (interactive)
+    (find-file (concat *my-pim-dir* "orgs/todo.org"))))
+(global-set-key [\C-f6]
+  (lambda ()
+    (interactive)
+    (find-file (concat *my-pim-dir* "orgs/work/aig/notes.org"))))
