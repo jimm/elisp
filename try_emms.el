da05cd2afc48b6ae86485b76c63a77c2aa4b0431
Jim Menard <jim@jimmenard.com>
2015-03-23 14:08:58 -0400

    removed old work stuff

--- try_emms.el
+++ try_emms.el
@@ -1,13 +1,6 @@
 (add-to-list 'load-path "/Users/jimm/src/emms/lisp/")
 (require 'emms-setup)
 (emms-standard)
-(emms-default-players)
-
-;; TODO Specific to my work setup. These rebindings should be moved into
-;; bootstrap/nrelate/widget/after.el.
-(when (fboundp 'vulcan-password-to-iterm)
-  (global-set-key [f5] 'vulcan-password-to-iterm)
-  (global-set-key [\C-f5] 'mindspark-password-to-clipboard))
 
 (global-set-key [f7] 'emms-previous)
 (global-set-key [f8] 'emms-pause)       ; toggles between pause and resume
