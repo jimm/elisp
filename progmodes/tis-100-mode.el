a754ec3c823a33c14cdc42aa3c7d16ed006639ac
Jim Menard <jim@jimmenard.com>
2015-07-13 11:17:22 -0400

    first draft of TIS-100 game save file/asm code generic mode

---
+++ progmodes/tis-100-mode.el
@@ -0,0 +1,26 @@
+;; See http://www.zachtronics.com/tis-100/
+
+;; (defface keymaster-note-face
+;;   '((t (:foreground "ForestGreen")))
+;;   "keymaster mode option face")
+
+(define-generic-mode tis-100-mode
+  '(";" )                               ; comment-list
+  '(                                    ; keyword-list
+    "NOP"
+    "MOV"
+    "SWP" "SAV"
+    "NEG"
+    "ADD" "SUB"
+    "JEZ" "JNZ" "JLZ" "JGZ" "JMP" "JRO"
+    )
+  '(					; font-lock-list
+    ("[0-9]+" . 'font-lock-constant-face)
+    )
+  '("\\.tis100$")                           ; auto-mode-list
+  '(
+;; (lambda () (auto-fill-mode t))
+)	;function-list
+  "Major mode for editing TIS-100 files.")
+
+(provide 'tis-100-mode)
