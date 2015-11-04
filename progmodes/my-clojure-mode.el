51006c9bd245cc0769ccd65b278ee0892176a05e
Jim Menard <jim@jimmenard.com>
2015-11-04 10:27:12 -0500

    Append to hooks, don't replace them

--- progmodes/my-clojure-mode.el
+++ progmodes/my-clojure-mode.el
@@ -24,15 +24,16 @@
 
 (unless-boundp-setq package-activated-list ())
 
-(setq clojure-mode-hook
+(add-hook 'clojure-mode-hook
       (lambda ()
         (when-fboundp-call inf-clojure-minor-mode)
         (define-key clojure-mode-map "\r" 'newline-and-indent)
+        (define-key clojure-mode-map "\C-c\C-c" #'comment-region)
         (define-key clojure-mode-map "\C-cd" 'debug-comment)
         (define-key clojure-mode-map "\C-ci" 'in-ns-to-inferior-lisp)
         (define-key clojure-mode-map "\C-cn" 'ns-to-inferior-lisp)))
 
-(setq nrepl-connected-hook
+(add-hook 'nrepl-connected-hook
       (lambda ()
         ;; nREPL mode has two key bindings that do the same thing: \C-c\C-c
         ;; and C-M-x both run nrepl-eval-expression-at-point. Normally
