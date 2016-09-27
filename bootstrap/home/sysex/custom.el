6b6112260e08bf29ca09679f87e6ca168b96e4d6
Jim Menard <jim@jimmenard.com>
2016-09-27 08:06:03 -0400

    Home config: added packages

--- bootstrap/home/sysex/custom.el
+++ bootstrap/home/sysex/custom.el
@@ -1,29 +1,29 @@
 (custom-set-variables
- '(abbrev-mode nil)                     ; this is the default default, anyway
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(abbrev-mode nil t)
  '(android-mode-sdk-dir "/usr/local/android-sdk-mac")
  '(css-indent-offset 2)
  '(eshell-save-history-on-exit t)
  '(package-selected-packages
    (quote
-    (yasnippet yaml-mode wttrin smex sicp sass-mode rspec-mode org-present markdown-mode magit less-css-mode inf-ruby inf-clojure http-twiddle haskell-mode hamlet-mode go-mode fzf flx-ido emms dumb-jump diminish deft coffee-mode bind-key alchemist ace-window 2048-game)))
- '(sgml-xml-mode t)
- '(woman-use-own-frame nil)
- '(sgml-xml-mode t)
+    (ag projectile projectile-rails yasnippet yaml-mode wttrin smex sicp sass-mode rspec-mode org-present markdown-mode magit less-css-mode inf-ruby inf-clojure http-twiddle haskell-mode hamlet-mode go-mode fzf flx-ido emms dumb-jump diminish deft coffee-mode bind-key alchemist ace-window 2048-game)))
  '(safe-local-variable-values
-   (quote ((org-publish-project-alist ("keymaster"
-                                       :base-directory "."
-                                       :publishing-directory "../public_html"
-                                       :style "<link rel=\"stylesheet\" href=\"style.css\" type=\"text/css\"/>"
-                                       :author "Jim Menard"
-                                       :email "jim@jimmenard.com"))
-           (org-publish-project-alist ("trackmaster"
-                                       :base-directory "."
-                                       :publishing-directory "../public_html"
-                                       :style "<link rel=\"stylesheet\" href=\"style.css\" type=\"text/css\"/>"
-                                       :author "Jim Menard"
-                                       :email "jim@jimmenard.com"))
-           (Syntax . Common-Lisp)))))
+   (quote
+    ((org-publish-project-alist
+      ("keymaster" :base-directory "." :publishing-directory "../public_html" :style "<link rel=\"stylesheet\" href=\"style.css\" type=\"text/css\"/>" :author "Jim Menard" :email "jim@jimmenard.com"))
+     (org-publish-project-alist
+      ("trackmaster" :base-directory "." :publishing-directory "../public_html" :style "<link rel=\"stylesheet\" href=\"style.css\" type=\"text/css\"/>" :author "Jim Menard" :email "jim@jimmenard.com"))
+     (Syntax . Common-Lisp))))
+ '(sgml-xml-mode t)
+ '(woman-use-own-frame nil))
 
 (custom-set-faces
- '(aw-leading-char-face
-   ((((class color)) (:foreground "red" :bold t)))))
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(aw-leading-char-face ((((class color)) (:foreground "red" :bold t))))
+ '(eshell-prompt ((((class color) (background light)) (:foreground "Blue")) (((class color) (background dark)) (:foreground "SteelBlue")) (t (:bold t)))))
