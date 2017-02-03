4234487a9e141076c7c8c551418845b62a0a7ab8
Jim Menard <jim@jimmenard.com>
2017-02-03 08:08:45 -0500

    new package

--- bootstrap/candi/ruby/init.el
+++ bootstrap/candi/ruby/init.el
@@ -9,7 +9,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (rust-mode elm-mode elm-yasnippets yasnippet yaml-mode textile-mode smex sicp sass-mode rspec-mode projectile-rails ponylang-mode org-present markdown-mode magit lua-mode less-css-mode inf-clojure http-twiddle haskell-mode hamlet-mode go-mode fzf flx-ido emms dumb-jump diminish deft coffee-mode bind-key alchemist ag ace-window 2048-game))))
+    (toml-mode rust-mode elm-mode elm-yasnippets yasnippet yaml-mode textile-mode smex sicp sass-mode rspec-mode projectile-rails ponylang-mode org-present markdown-mode magit lua-mode less-css-mode inf-clojure http-twiddle haskell-mode hamlet-mode go-mode fzf flx-ido emms dumb-jump diminish deft coffee-mode bind-key alchemist ag ace-window 2048-game))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
