cd547d8397722b32f6cdfec0c8c7cbb5780d97df
Jim Menard <jim@jimmenard.com>
2017-02-15 07:25:33 -0500

    org face tweak

--- emacs.el
+++ emacs.el
@@ -1317,7 +1317,7 @@ values."
             (define-key yas-keymap "\t" 'yas-next-field-or-maybe-expand)))
 
 (set-face-attribute 'org-level-1 nil :height 1.2 :bold t)
-(set-face-attribute 'org-level-2 nil :foreground "darkbrown" :bold t)
+(set-face-attribute 'org-level-2 nil :foreground "black" :bold t)
 (set-face-attribute 'org-block nil :foreground "black")
 (setq org-fontify-whole-heading-line t) ; bg color covers whole line
 
