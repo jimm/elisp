3d902d46d2d16c211bd451ebb0149118689e2c89
Jim Menard <jim@jimmenard.com>
2017-02-17 09:34:19 -0500

    check for Emacs version before using org-block

--- emacs.el
+++ emacs.el
@@ -1318,7 +1318,8 @@ values."
 
 (set-face-attribute 'org-level-1 nil :height 1.2 :bold t)
 (set-face-attribute 'org-level-2 nil :foreground "black" :bold t)
-(set-face-attribute 'org-block nil :foreground "black")
+(when (>= emacs-major-version 24)
+  (set-face-attribute 'org-block nil :foreground "black"))
 (setq org-fontify-whole-heading-line t) ; bg color covers whole line
 
 ;;
