7c33ea6cda301ad202ccf081864d4211e8942c94
Jim Menard <jim@jimmenard.com>
2017-04-12 13:17:47 -0400

    dumb jump

--- emacs.el
+++ emacs.el
@@ -185,7 +185,9 @@ whitespace-only string."
   :bind (("M-g o" . dumb-jump-go-other-window)
          ("M-g j" . dumb-jump-go)
          ("M-g k" . dumb-jump-back)
-         ("M-g q" . dumb-jump-quick-look)))
+         ("M-g q" . dumb-jump-quick-look))
+  :config
+  (setq dumb-jump-functions-only t))
 
 (use-package elixir-mode
   :init
