931760139e0767369cb16509f23ff2c501e35313
Jim Menard <jim@jimmenard.com>
2017-10-06 08:31:33 -0400

    update

--- emacs.el
+++ emacs.el
@@ -37,7 +37,10 @@ whitespace-only string."
        (list start end))))
 
 (when (fboundp #'package-initialize)
-  (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+  (let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
+                    (not (gnutls-available-p))))
+       (url (concat (if no-ssl "http" "https") "://melpa.org/packages/")))
+  (add-to-list 'package-archives (cons "melpa" url) t))
   (when (< emacs-major-version 24)
     ;; For important compatibility libraries like cl-lib
     (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
