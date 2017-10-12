307a3e774669d93692f03403c8fec4af59148947
Jim Menard <jim.menard@chloeandisabel.com>
2017-10-12 16:55:07 -0400

    added graphviz-mode

--- bootstrap/candi/gem/custom.el
+++ bootstrap/candi/gem/custom.el
@@ -8,7 +8,7 @@
  '(eshell-save-history-on-exit t)
  '(package-selected-packages
    (quote
-    (graphviz-dot-mode yasnippet yasnippet-snippets inf-ruby dockerfile-mode magit-gitflow rjsx-mode magit toml-mode rust-mode elm-mode elm-yasnippets yaml-mode textile-mode smex sicp sass-mode rspec-mode projectile-rails ponylang-mode org-present markdown-mode lua-mode less-css-mode inf-clojure http-twiddle haskell-mode hamlet-mode go-mode fzf flx-ido emms dumb-jump diminish coffee-mode bind-key alchemist ag ace-window 2048-game)))
+    (slime graphviz-dot-mode yasnippet yasnippet-snippets inf-ruby dockerfile-mode magit-gitflow rjsx-mode magit toml-mode rust-mode elm-mode elm-yasnippets yaml-mode textile-mode smex sicp sass-mode rspec-mode projectile-rails ponylang-mode org-present markdown-mode lua-mode less-css-mode inf-clojure http-twiddle haskell-mode hamlet-mode go-mode fzf flx-ido emms dumb-jump diminish coffee-mode bind-key alchemist ag ace-window 2048-game)))
  '(sgml-xml-mode t)
  '(woman-use-own-frame nil))
 (custom-set-faces
