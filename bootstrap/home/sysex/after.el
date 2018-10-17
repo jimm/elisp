dd1870190bb52de27f5447b69246e2cec12ee7e4
Jim Menard <jim@jimmenard.com>
2018-10-17 11:53:31 -0400

    a bit of refactoring

--- bootstrap/home/sysex/after.el
+++ bootstrap/home/sysex/after.el
@@ -1,18 +1,12 @@
 (lighten-up)
 
-(defvar work-orgs-dir "candi"
+(defvar work-orgs-dir "warby_parker"
   "Name of $pim/orgs/work subdir where I keep work-related Org mode files.")
 
 (setq dired-use-ls-dired nil)
 
 (setq browse-url-generic-program "open")
 (setq Man-switches "-M /usr/share/man:/usr/local/share/man")
-
-(defvar compile-ant "ant -e -s build.xml ")
-(defvar compile-rake "rake test ")
-
-(set-register ?n compile-ant)
-(set-register ?q "rake test ")
 
 (setq sql-sqlite-program "sqlite3")
 
