8c84462bf51c673aad83837367d660cbbdb02429
Jim Menard <jim@jimmenard.com>
2019-01-24 14:12:57 -0500

    remove projectile at work

--- bootstrap/warby_parker/lens/after.el
+++ bootstrap/warby_parker/lens/after.el
@@ -46,16 +46,16 @@ be found in `wp-pr-abbreviations-alist'."
 ;;; projectile-project-root returns nil if no dir found, but the original
 ;;; definition of fzf does not handle that.
 (require 'fzf)
-(defun fzf ()
-  "Starts a fzf session."
-  (interactive)
-  (if (fboundp #'projectile-project-root)
+(if (fboundp #'projectile-project-root)
+    (defun fzf ()
+      "Starts a fzf session."
+      (interactive)
       (fzf/start (or (condition-case err
                          (projectile-project-root)
                        (error
                         default-directory))
                      default-directory))
-    (fzf/start default-directory)))
+      (fzf/start default-directory)))
 
 ;;; ================================================================
 
