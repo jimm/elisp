03227fc5294d0424023ef57e497fb770c5cea9ff
Jim Menard <jim@jimmenard.com>
2019-02-05 11:15:47 -0500

    new function sends command + file name to iTerm

--- iterm.el
+++ iterm.el
@@ -1,14 +1,15 @@
-(defun send-to-iterm (str)
+(defun send-to-iterm (str &optional suppress-newline)
   "Send STR to the front window/session in iTerm, with an
-additional newline if STR does not end in one. STR may contain
-multiple lines."
+additional newline if STR does not end in one (and if
+SUPPRESS-NEWLINE is not nil). STR may contain multiple lines."
   (interactive "siTerm input: ")
   (let ((tempfile (concat "/tmp/emacs_iterm_"
                           (int-to-string (abs (random)))
                           ".txt")))
     (with-temp-file tempfile
       (insert str)
-      (unless (string-equal "\n" (substring str (1- (length str))))
+      (unless (or suppress-newline
+                  (string-equal "\n" (substring str (1- (length str)))))
         (insert "\n")))
     (do-applescript (concat
                      "tell application \"iTerm\"\n"
@@ -30,6 +31,12 @@ multiple lines."
   "Send the current buffer to iTerm using send-to-iterm."
   (interactive)
   (send-to-iterm (buffer-substring-no-properties (point-min) (point-max))))
+
+(defun send-buffer-file-to-iterm (command)
+  "Send COMMAND + the (properly quoted quoted) file name of the
+current buffer to iTerm using send-to-iterm."
+  (interactive "sCommand: ")
+  (send-to-iterm (concat command " " (shell-quote-argument buffer-file-name))))
 
 (defun send-current-line-to-iterm ()
   "Send the current line to iTerm using `send-to-iterm'. See also
