25bf6b527f15d63dd20c5e9ff6ab4a96a128d6fc
Jim Menard <jim@jimmenard.com>
2019-03-07 16:34:13 -0500

    fullscreen

--- bootstrap/warby_parker/lens/before.el
+++ bootstrap/warby_parker/lens/before.el
@@ -6,10 +6,9 @@
 (when window-system
   (let ((basic-frame-alist '((tool-bar-mode . nil)
                              (font . "Menlo 13"))))
-    (setq initial-frame-alist
-          (append basic-frame-alist '((width . 176) (height . 56)
-                                      (top . 0) (left . 0)))
-          default-frame-alist basic-frame-alist)))
+    (setq
+     initial-frame-alist (append basic-frame-alist '((fullscreen . maximized)))
+     default-frame-alist basic-frame-alist)))
 
 (defvar *my-erlang-emacs-tools-dir*
   (concat (car (file-expand-wildcards "/usr/local/lib/erlang/lib/tools-*"))
