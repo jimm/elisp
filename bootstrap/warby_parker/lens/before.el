036d1416efb9495b7beeb9b8a0a0f2aa9b563452
Jim Menard <jim.menard@warbyparker.com>
2020-08-13 11:53:46 -0400

    change initial frame size

--- bootstrap/warby_parker/lens/before.el
+++ bootstrap/warby_parker/lens/before.el
@@ -7,7 +7,7 @@
   (let ((basic-frame-alist '((tool-bar-mode . nil)
                              (font . "Menlo 13"))))
     (setq
-     initial-frame-alist (append basic-frame-alist '((fullscreen . maximized)))
+     initial-frame-alist (append basic-frame-alist '((fullscreen . fullheight)))
      default-frame-alist basic-frame-alist)))
 
 (defvar *my-erlang-emacs-tools-dir*
