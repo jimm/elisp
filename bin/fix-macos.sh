6ba0edf0443fd0562567293a3627eb721c99200a
Jim Menard <jim@jimmenard.com>
2021-04-21 09:11:47 -0400

    fix-macos.sh not needed as of 27.2-2 build from emacsformacosx.com

--- bin/fix-macos.sh
+++ bin/fix-macos.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 #
-# See https://spin.atomicobject.com/2019/12/12/fixing-emacs-macos-catalina/
+# usage: fix-macos.sh [-f]
 #
+# Note: this script is no longer necessary as of the
+# https://emacsformacosx.com/ build of Emacs 27.2-2 (and possibly earlier).
+#
+# See https://spin.atomicobject.com/2019/12/12/fixing-emacs-macos-catalina/
 # This version contains an update mentioned in the comments.
 
 MACOS_DIR=/Applications/Emacs.app/Contents/MacOS
 EMACS_EXE_PREFIX=Emacs-x86_64-
+
+if [ "$1" != "-f" ] ; then
+    echo "warning: this script isn't necessary for Emacs v27.2-2 and higher"
+    echo "re-run with '-f' to ignore this warning and run this script"
+    exit 1
+fi
 
 if [ ! -d "$MACOS_DIR" ] ; then
     echo error: "$MACOS_DIR" does not exist, exiting
