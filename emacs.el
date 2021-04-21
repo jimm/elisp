40d64b299df1bda0ba4e841ae63b063d4aea95c0
Jim Menard <jim@jimmenard.com>
2021-04-21 09:11:33 -0400

    minor package initialization improvement

--- emacs.el
+++ emacs.el
@@ -38,17 +38,14 @@ whitespace-only string."
        (list start end))))
 
 (when (fboundp #'package-initialize)
-  (let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
-                    (not (gnutls-available-p))))
-       (url (concat (if no-ssl "http" "https") "://melpa.org/packages/")))
-    (add-to-list 'package-archives (cons "melpa" url) t))
+  (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
   (when (< emacs-major-version 24)
     ;; For important compatibility libraries like cl-lib
     (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
   (when (< emacs-major-version 27)
-    (setq package-enable-at-startup nil ; prevent initializing twice
-          package--init-file-ensured t) ; avoid check for being in init.el
-    (package-initialize)))
+    (setq package-enable-at-startup nil  ; prevent initializing twice
+          package--init-file-ensured t)) ; avoid check for being in init.el
+  (package-initialize))
 
 (defun my-start-shell ()
   (interactive)
