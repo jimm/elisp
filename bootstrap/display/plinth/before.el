9527260d69631f004171261a679563342d7f6051
Jim Menard <jim@jimmenard.com>
2022-08-19 09:00:26 -0400

    font initialization tweak

--- bootstrap/display/plinth/before.el
+++ bootstrap/display/plinth/before.el
@@ -6,6 +6,7 @@
 (when window-system
   (let ((basic-frame-alist '((tool-bar-mode . nil)
                              (font . "Menlo 13"))))
+    (set-face-attribute 'default t :font "Menlo 13")
     (setq
      initial-frame-alist basic-frame-alist
      default-frame-alist basic-frame-alist)))
