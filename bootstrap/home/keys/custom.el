8a85399eb935f82daa68f1e436d13c19e9b087c1
Jim Menard <jim@jimmenard.com>
2024-06-09 22:06:20 -0400

    update

--- bootstrap/home/keys/custom.el
+++ bootstrap/home/keys/custom.el
@@ -7,10 +7,10 @@
  '(android-mode-sdk-dir "/usr/local/android-sdk-mac")
  '(css-indent-offset 2)
  '(custom-safe-themes
-   '("a0582db84985ddb3468e5552425779b9f1483d352861e836b3265e49ab078217" "45e9de5ee4432590ebf4defc4882e1a7b813bba22716e0043b356c99abc63465" default))
+   '("6018fc37be706648313ac42aa9c6759e44c72357ab6f8ba83b93752f066a3c05" "a0582db84985ddb3468e5552425779b9f1483d352861e836b3265e49ab078217" "45e9de5ee4432590ebf4defc4882e1a7b813bba22716e0043b356c99abc63465" default))
  '(eshell-save-history-on-exit t)
  '(package-selected-packages
-   '(go-snippets py-isort python-black rubocopfmt jade-mode typescript-mode org-present swift-mode avy ox-impress-js projectile projectile-rails slime suggest yasnippet-snippets lua-mode crystal-mode graphviz-dot-mode textile-mode use-package ag yasnippet yaml-mode smex sicp sass-mode rspec-mode markdown-mode magit less-css-mode inf-ruby inf-clojure http-twiddle haskell-mode hamlet-mode go-mode fzf flx-ido dumb-jump diminish coffee-mode alchemist ace-window 2048-game))
+   '(go-snippets py-isort python-black rubocopfmt jade-mode typescript-mode org-present avy ox-impress-js projectile-rails slime suggest yasnippet-snippets lua-mode crystal-mode graphviz-dot-mode textile-mode use-package ag smex sicp sass-mode rspec-mode magit less-css-mode inf-ruby inf-clojure http-twiddle haskell-mode hamlet-mode go-mode fzf flx-ido dumb-jump diminish coffee-mode alchemist ace-window 2048-game))
  '(python-fill-docstring-style 'pep-257-nn)
  '(sgml-xml-mode t)
  '(woman-use-own-frame nil))
