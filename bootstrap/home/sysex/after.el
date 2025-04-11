c3adc247f1f2d16b2b99fe9ffc5f8780c15a85c3
Jim Menard <jim@jimmenard.com>
2025-04-11 12:45:53 -0400

    man path, a few more var tweaks

--- bootstrap/home/sysex/after.el
+++ bootstrap/home/sysex/after.el
@@ -1,8 +1,9 @@
 (load-theme 'jim-light)
 
-(setq dired-use-ls-dired nil
+(setq user-email-address "jim@jimmenard.com"
+      dired-use-ls-dired nil
       browse-url-generic-program "open"
-      Man-switches "-M /usr/share/man:/usr/local/share/man"
+      Man-switches "-M /usr/share/man:/opt/homebrew/share/man"
       sql-sqlite-program "sqlite3"
       rubocopfmt-rubocop-command "rubocop-daemon-wrapper")
 
