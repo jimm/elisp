ee937e10187de93692f65e582cc60f5e4519d470
Jim Menard <jim@jimmenard.com>
2025-04-22 14:41:04 -0400

    markdown mode for home

--- bootstrap/home/sysex/custom.el
+++ bootstrap/home/sysex/custom.el
@@ -7,9 +7,9 @@
    '("b6c577260232a8b6032ce5db36d3d805e40d6da6f5cf15b847b9fdc810dc87bd" default))
  '(package-selected-packages
    '(ace-window ag alchemist alchemist-mode dash diminish elixir elixir-mode
-                flx-ido fzf go-mode janet-mode lua-mode magit mood-line
-                mood-line-mode rubocopfmt ruff-format smex yasnippet
-                yasnippet-snippets))
+                flx-ido fzf go-mode janet-mode lua-mode magit markdown-mode
+                mood-line mood-line-mode rubocopfmt ruff-format smex
+                yasnippet yasnippet-snippets))
  '(python-fill-docstring-style 'pep-257-nn))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
