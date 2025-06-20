90f483301d24aa75ee052490143d0175539d2071
Jim Menard <jim@jimmenard.com>
2025-06-19 20:23:17 -0400

    custom mods for one config

--- bootstrap/opalstack/jimm/custom.el
+++ bootstrap/opalstack/jimm/custom.el
@@ -1,11 +1,22 @@
 (custom-set-variables
- '(abbrev-mode nil)                     ; this is the default default, anyway
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(abbrev-mode nil t)
  '(css-indent-offset 2)
+ '(epg-gpg-program "/usr/bin/gpg2")
  '(eshell-save-history-on-exit t)
+ '(package-selected-packages
+   '(smex fzf mood-line ace-window yasnippet-snippets yasnippet rubocopfmt ruff-format alchemist elixir-mode dash flx-ido use-package magit))
+ '(python-fill-docstring-style 'pep-257-nn)
  '(sgml-xml-mode t)
- '(woman-use-own-frame nil)
- '(sgml-xml-mode t))
+ '(woman-use-own-frame nil))
 
 (custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
  '(aw-leading-char-face ((((class color)) (:foreground "red" :bold t))))
  '(eshell-prompt ((((class color) (background light)) (:foreground "Blue")) (((class color) (background dark)) (:foreground "SteelBlue")) (t (:bold t)))))
