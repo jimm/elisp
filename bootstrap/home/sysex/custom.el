fae72066cf0d4616cb9ea270a01f7fbc3c764bd0
Jim Menard <jim@jimmenard.com>
2025-07-03 14:59:40 -0400

    accept .dir-local.el var

--- bootstrap/home/sysex/custom.el
+++ bootstrap/home/sysex/custom.el
@@ -15,7 +15,9 @@
                 yasnippet yasnippet-snippets))
  '(python-fill-docstring-style 'pep-257-nn)
  '(safe-local-variable-values
-   '((*my-org-mode-pr-repo-abbrevs-alist* ("q" . "quark") ("p" . "proton")
+   '((*my-org-mode-tk-link-prefix*
+      . "https://app.shortcut.com/particlehealth/story/")
+     (*my-org-mode-pr-repo-abbrevs-alist* ("q" . "quark") ("p" . "proton")
                                           ("g" . "gluon"))
      (*my-org-mode-repo-link-prefix* . "https://github.com/ParticleHealth/"))))
 (custom-set-faces
