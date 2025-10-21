9493ed72809a8b50a2a303c51f1993476b0c8f3f
Jim Menard <jim@jimmenard.com>
2025-10-21 18:22:23 -0400

    added markdown mode

--- bootstrap/home/sysex/custom.el
+++ bootstrap/home/sysex/custom.el
@@ -11,8 +11,8 @@
  '(package-selected-packages
    '(2048-game ace-window ag alchemist browse-at-remote claude-code diminish
                eat flx-ido fzf go-mode janet-mode kotlin-mode lua-mode magit
-               mermaid-mode mood-line rubocopfmt ruff-format smex
-               terraform-mode yaml-mode yasnippet-snippets))
+               markdown-mode mermaid-mode mood-line rubocopfmt ruff-format
+               smex terraform-mode yaml-mode yasnippet-snippets))
  '(package-vc-selected-packages
    '((claude-code :url "https://github.com/stevemolitor/claude-code.el")))
  '(python-fill-docstring-style 'pep-257-nn)
