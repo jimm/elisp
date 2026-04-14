6de6c072c1501db6a902ba9ca559f0553c17f38d
Jim Menard <jim.menard@therealreal.com>
2026-04-14 16:48:30 -0400

    package updates

--- bootstrap/trr/lfo/custom.el
+++ bootstrap/trr/lfo/custom.el
@@ -7,12 +7,15 @@
  '(custom-safe-themes
    '("c047f43f076fab9232aa6766caba661a912b5c5a5da0f5ed1926061cedf0393d" default))
  '(package-selected-packages
-   '(ace-window alchemist browse-at-remote dash elixir-mode flx-ido fzf magit
-                magnus mood-line rubocopfmt ruff-format smex yasnippet
-                yasnippet-snippets))
+   '(ace-window alchemist browse-at-remote dash diminish elixir-mode flx-ido
+                fzf htmlize magit magnus markdown-mode mood-line projectile
+                projectile-rails rspec-mode rubocopfmt ruff-format slime
+                smex suggest yasnippet yasnippet-snippets))
  '(python-fill-docstring-style 'pep-257-nn)
  '(safe-local-variable-values
-   '((*my-org-mode-tk-link-prefix* . "https://trr-prod.atlassian.net/browse/")
+   '((*my-org-mode-repo-link-prefix* . "https://github.com/TheRealReal/")
+     (*my-org-mode-tk-link-prefix*
+      . "https://trr-prod.atlassian.net/browse/")
      (*my-org-mode-pr-repo-abbrevs-alist* quote (("s" . "seller")))
      (*my-org-mode-repo-link-prefix* . "https://github.com/TheRealReal"))))
 (custom-set-faces
