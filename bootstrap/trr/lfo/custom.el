1e547eb80e7a0519cdca65fac668cdd7c9c9c4bf
Jim Menard <jim.menard@therealreal.com>
2026-04-15 12:51:03 -0400

    YAPI (yet another package installation)

--- bootstrap/trr/lfo/custom.el
+++ bootstrap/trr/lfo/custom.el
@@ -7,10 +7,10 @@
  '(custom-safe-themes
    '("c047f43f076fab9232aa6766caba661a912b5c5a5da0f5ed1926061cedf0393d" default))
  '(package-selected-packages
-   '(ace-window alchemist browse-at-remote dash diminish elixir-mode flx-ido
-                fzf htmlize magit magnus markdown-mode mood-line projectile
-                projectile-rails rspec-mode rubocopfmt ruff-format slime
-                smex suggest yasnippet yasnippet-snippets))
+   '(2048-game ace-window alchemist browse-at-remote dash diminish elixir-mode
+               flx-ido fzf htmlize magit magnus markdown-mode mood-line
+               projectile projectile-rails rspec-mode rubocopfmt ruff-format
+               slime smex suggest yasnippet yasnippet-snippets))
  '(python-fill-docstring-style 'pep-257-nn)
  '(safe-local-variable-values
    '((*my-org-mode-repo-link-prefix* . "https://github.com/TheRealReal/")
