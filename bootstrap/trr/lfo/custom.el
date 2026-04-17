76558d874ba48f8af3de6117b32cd8f5604f6dcb
Jim Menard <jim.menard@therealreal.com>
2026-04-17 08:41:47 -0400

    update

--- bootstrap/trr/lfo/custom.el
+++ bootstrap/trr/lfo/custom.el
@@ -8,12 +8,23 @@
    '("c047f43f076fab9232aa6766caba661a912b5c5a5da0f5ed1926061cedf0393d" default))
  '(package-selected-packages
    '(2048-game ace-window alchemist browse-at-remote dash diminish elixir-mode
-               flx-ido fzf htmlize magit magnus markdown-mode mood-line
-               projectile projectile-rails rspec-mode rubocopfmt ruff-format
-               slime smex suggest yasnippet yasnippet-snippets))
+               flx-ido fzf htmlize lua-mode magit magnus markdown-mermaid
+               markdown-mode mermaid-mode mood-line projectile
+               projectile-rails rspec-mode rubocopfmt ruff-format slime smex
+               suggest vterm yasnippet yasnippet-snippets))
  '(python-fill-docstring-style 'pep-257-nn)
  '(safe-local-variable-values
-   '((*my-org-mode-repo-link-prefix* . "https://github.com/TheRealReal/")
+   '((*my-org-mode-pr-repo-abbrevs-alist* ("a" . "api") ("as" . "admin-sales")
+                                          ("gp" . "grcp-packages")
+                                          ("rs" . "real-server")
+                                          ("se" . "seller")
+                                          ("su" . "supply")
+                                          ("tc" . "trr-compose")
+                                          ("wj" . "website-jamstack"))
+     (*my-org-mode-pr-repo-abbrevs-alist* quote
+                                          (("s" . "seller") ("a" . "api")
+                                           ("tc" . "trr-compose")))
+     (*my-org-mode-repo-link-prefix* . "https://github.com/TheRealReal/")
      (*my-org-mode-tk-link-prefix*
       . "https://trr-prod.atlassian.net/browse/")
      (*my-org-mode-pr-repo-abbrevs-alist* quote (("s" . "seller")))
