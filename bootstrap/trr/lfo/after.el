f950a70ecba8c918c8cc337fdfd27c6ca6f0879f
Jim Menard <jim.menard@therealreal.com>
2026-07-23 17:26:31 -0400

    comment, experiement

--- bootstrap/trr/lfo/after.el
+++ bootstrap/trr/lfo/after.el
@@ -5,11 +5,40 @@
       browse-url-generic-program "open"
       Man-switches "-M /usr/share/man:/opt/homebrew/share/man"
       sql-sqlite-program "sqlite3"
-      rubocopfmt-rubocop-command "rubocop-daemon-wrapper"
+      rubocopfmt-rubocop-command "rubocop-daemon-wrapper" ; overridden in $trr/.dir-locals.el
       my-shell #'shell
       my-alternate-shell #'eshell)
 
 ;; ;;; experimentation
+
+;; (defvar trr-link-regexp
+;;   "Regular expression matching a URL that should go to my work browser."
+;;   (rx (or "therealreal"
+;;           "TheRealReal"
+;;           "app.notion.com"
+;;           "bugsnag"
+;;           "datadog")))
+
+;; (defun trr-link-p (link)
+;;   (and (org-url-p link)
+;;        (string-match-p trr-link-regexp link)))
+
+;; (defun my-org-maybe-open-url (&optional arg)
+;;   "A hook for opening URLs in the appropriate browser: Chrome for work, Firefox for other.
+
+;; See `org-open-at-point-functions'."
+;;   (let ((link (org--link-at-point)))
+;;     (message "Org link at point: \"%s\"" (org--link-at-point))
+;;     (cond ((trr-link-p link)
+;;            (start-process (concat "open " link) nil "open" "-a" "Chrome" link))
+;;           ((org-url-p link)
+;;            (start-process (concat "open " link) nil "open" "-a" "Firefox" link)))))
+
+;; (add-hook 'org-open-at-point-functions #'my-org-maybe-open-url)
+
+
+
+
 
 ;; (defun trr-url-p (url)
 ;;   "Returns non-nil if `url` is a work URL and needs to be opened with the
