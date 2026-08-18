ba1e5f929d41fa46f544a4600f39bd36e88b148a
Jim Menard <jim.menard@therealreal.com>
2026-08-18 09:35:16 -0400

    unfill-paragraph is coming in Emacs 31.1.

--- emacs.el
+++ emacs.el
@@ -1416,13 +1416,17 @@ found in the config file."
 ;;; Stefan Monnier <foo at acm.org>. It is the opposite of fill-paragraph
 ;;; Found on the Emacs Wiki at
 ;;; https://www.emacswiki.org/emacs/UnfillParagraph
-(defun unfill-paragraph (&optional region)
-  "Takes a multi-line paragraph and makes it into a single line of text."
-  (interactive (progn (barf-if-buffer-read-only) '(t)))
-  (let ((fill-column (point-max))
-        ;; This would override `fill-column' if it's an integer.
-        (emacs-lisp-docstring-fill-column t))
-    (fill-paragraph nil region)))
+;;;
+;;; Note: unfill-paragraph is being included as of Emacs 31.1. Thus the
+;;; check for its existence.
+(unless (fboundp #'unfill-paragraph)
+  (defun unfill-paragraph (&optional region)
+    "Takes a multi-line paragraph and makes it into a single line of text."
+    (interactive (progn (barf-if-buffer-read-only) '(t)))
+    (let ((fill-column (point-max))
+          ;; This would override `fill-column' if it's an integer.
+          (emacs-lisp-docstring-fill-column t))
+      (fill-paragraph nil region))))
 
 (defalias #'unwrap #'unfill-paragraph)
 
